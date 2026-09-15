e527a665b673b5802af8090a2ce5fb13d11f1a08
byteMonk <bytemonk71@gmail.com>
2026-09-15 21:11:56 +0530

    redis list

---
+++ list.js
@@ -0,0 +1,18 @@
+// Redis lists are linked lists of string values. Redis lists are frequently used to:
+// implement stack and queues.
+// Build queue management for background worker systems!
+
+const client = require("./client");
+
+async function init() {
+    // await client.lpush("messages", 1);
+    // await client.lpush("messages", 2);
+    // await client.lpush("messages", 3);
+    // await client.lpush("messages", 4);
+    // const result = await client.rpop("messages");
+    const result1 = await client.blpop("messages", 10);
+    console.log(result1);
+    
+}
+
+init();
