580e823b03d227e89afefd13b4364e4d294b3f26
byteMonk <bytemonk71@gmail.com>
2026-09-15 21:15:48 +0530

    TTL: 17secs

--- string.js
+++ string.js
@@ -3,6 +3,7 @@ const client = require("./client");
 
 async function init() {
     await client.set("msg:3", "hello from Nodejs");
+    await client.expire("msg:3", 17);
     const result = await client.get("msg:3");
     console.log("Result ->", result);
 }
